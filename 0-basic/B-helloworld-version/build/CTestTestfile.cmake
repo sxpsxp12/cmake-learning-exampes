11c0991a2a8ca8b458d3f5af99fcf9c9dd8ecb29
songxiaopeng <songxp@smartbow.net>
2020-01-29 20:24:22 +0800

    fix: 删除无用目录 --user=sxpsxp12

--- 0-basic/B-helloworld-version/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/songxp/program/cmake-learning-exampes/0-basic/A-helloworld-cmake
-# Build directory: /home/songxp/program/cmake-learning-exampes/0-basic/A-helloworld-cmake/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- 0-basic/B-helloworld-version/build/version.h
+++
@@ -1,3 +0,0 @@
-// the configured options and settings for helloworld
-#define Helloworld_VERSION_MAJOR 1
-#define Helloworld_VERSION_MINOR 0
